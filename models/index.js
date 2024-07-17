e7b6cf5e81576301734285c061746baf11d2515e
itspancakeman <collinjst.onge@gmail.com>
2024-07-16 23:52:44 -0400

    feat: connecting mongodb and updating package.json

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const uri = process.env.MONGO_URI
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+mongoose.connect(process.env.MONGO_URI);
+
+const db = mongoose.connection;
+
+db.once('open', () => console.log(`Connected to MongoDB at ${db.host}:${db.port}`));
+db.on('error', (error) => console.log('Database error\n', error));
