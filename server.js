6f51a4f31ccacc514c40c8fb9a4a6c50d9eba0a2
itspancakeman <collinjst.onge@gmail.com>
2024-07-16 23:33:19 -0400

    chores: installed dependancies, added .env, .gitignore, and server.js, established some global variables, setup some middleware, and readied server listener

---
+++ server.js
@@ -0,0 +1,25 @@
+// ===== GLOBAL VARIABLES ===== 
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const PORT = process.env.PORT || 3000;
+const methodOverride = require('method-override');
+const axios = require('axios');
+const cors = require('cors');
+
+// ====== MIDDLEWARE ======
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(cors())
+
+
+
+// ===== SERVER LISTENER ===== 
+const server = app.listen(PORT, () => {
+    console.log('listening at PORT ', PORT);
+});
+
+module.exports = server;
