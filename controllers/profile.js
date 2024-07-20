2a4e1c1f98ed4f344d9bf4257e5f5d3c2d7554b7
rSchleckser <rickyschleckser@gmail.com>
2024-07-20 15:21:12 -0700

    feat: identify user when accessing profile, on profile page

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,11 +3,10 @@ const User = require('../models/user');
 // @desc GET - User Profile
 const getUserProfile = async (req, res) => {
     try {
-        // const user = await User.findById(req.params.userId).select('-password'); // Exclude password from the response
-        // if (!user) {
-        //     return res.status(404).json({ msg: 'User not found' });
-        // }
-        res.status(200).send('Pass');
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+
+        // res.status(200).send('Pass');
     } catch (error) {
         res.status(500).json({ msg: 'Server Error', error: error.message });
     }
