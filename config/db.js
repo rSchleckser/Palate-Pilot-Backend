3f357cb9fe4d84466ff0da4570499a8d4e828ec5
rSchleckser <rickyschleckser@gmail.com>
2024-07-20 10:09:57 -0700

    feat: update

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
 const connectDB = async () =>{
     try {
+        
        await mongoose.connect(process.env.MONGO_URI);
        console.log('MongoDB Connected')
     } catch (error) {
