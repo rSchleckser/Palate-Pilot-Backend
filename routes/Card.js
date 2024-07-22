b0b07d86be116619b84215e5943eef474f5c7734
itspancakeman <collinjst.onge@gmail.com>
2024-07-21 22:07:30 -0400

    fix: card route

--- routes/Card.js
+++ routes/Card.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Card = require('../models/Card');
+const card = require('../models/Card')
 
 
 
-outer.get('/Card', isLoggedIn, (req, res) => {
-  const Card = req.user.Card;
+router.get('/cards', (req, res) => {
+  let cards = req.body.card;
+  
+  res.json(cards);
 
-  try {
-      res.status(200).send(Card);
-  } catch (error) {
-      res.status(404).render('404');
-  }
 });
 
 module.exports = 
