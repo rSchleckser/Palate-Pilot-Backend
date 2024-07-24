e9d590499763d5b5ba6aa6d7f6eeeabf939cd58d
itspancakeman <collinjst.onge@gmail.com>
2024-07-24 12:42:37 -0400

    feat: latest update

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,15 +1,15 @@
-// const Food = require('../models/Food');
-const foods  = require('../data/food');
+const Food = require('../models/Food');
+// const foods  = require('../data/food');
 
-exports.getFood = async (req, res) => {
+getFood = async (req, res) => {
     try {
-        // const food = await Food.findById(req.food.name);
-        // res.json(food);
-        
-        console.log(foods)
-        res.json({foods})
+        const food = await Food.find({ name: {$gt: 0} });
+        console.log(food)
+        res.send(food);
+
     } catch (error) {
         res.status(500).json({ msg: 'Server Error', error: error.message });
     }
 };
 
+module.exports = getFood;
